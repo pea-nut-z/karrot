df7bc37c22cbfbc6bb44c9395d0cb3b7dd3dbdc9
pea-nut-z <topeanutz@gmail.com>
2022-05-03 18:19:35 -0400

    removed engines and resolved image reference conflict in mobile vs web

--- screens/ItemDetails.js
+++ screens/ItemDetails.js
@@ -31,8 +31,6 @@ export default function ItemDetails({ route, navigation }) {
   });
 
   const images = item.images;
-  // console.log({ images });
-
   const useImgStyle = typeof item.images[0] === "number" ? false : true;
 
   // USER'S FAVOURITES
