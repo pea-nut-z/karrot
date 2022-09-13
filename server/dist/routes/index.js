6eae573314ab94c781ddc2657393aec9846692d7
pea-nut-z <topeanutz@gmail.com>
2022-09-13 19:07:10 -0400

    fix: remove dis from gitignore

---
+++ server/dist/routes/index.js
@@ -0,0 +1,7 @@
+import account from "./account.js";
+import review from "./review.js";
+import activity from "./activity.js";
+import listing from "./listing.js";
+import profile from "./profile.js";
+import restrict from "./restrict.js";
+export { account, activity, review, listing, profile, restrict };
