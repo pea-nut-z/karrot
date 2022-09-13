6e53c1666b53f75067b956444a012945d46c3797
pea-nut-z <topeanutz@gmail.com>
2022-09-13 19:14:04 -0400

    fix: remove dis dir

--- server/dist/model/index.js
+++
@@ -1,5 +0,0 @@
-import Restriction from "./Restriction.js";
-import Account from "./Account.js";
-import Activity from "./Activity.js";
-import Review from "./Review.js";
-export { Restriction, Account, Activity, Review };
--- server/dist/routes/index.js
+++
@@ -1,7 +0,0 @@
-import account from "./account.js";
-import review from "./review.js";
-import activity from "./activity.js";
-import listing from "./listing.js";
-import profile from "./profile.js";
-import restrict from "./restrict.js";
-export { account, activity, review, listing, profile, restrict };
