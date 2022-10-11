f4c8ba22baa1143f1bf26b791caea51904365c73
Peanut <topeanutz@gmail.com>
2022-10-10 21:11:27 -0400

    feat: add Edit Post Modal variables

--- client/src/variables.js
+++ client/src/variables.js
@@ -8,20 +8,42 @@ export const detailedItemStatusOptions = {
     Hidden: ["Edit", "Delete"]
   }
   
-  export const alertOptions = {
+export const alertOptions = {
+    title: {
+        message: "Enter a title"
+    },
+    category: {
+        message: "Select a category"
+    },
+    description: {
+        message: "Enter a description"
+    },
+    descLength: {
+        message: "Tell us a bit more for description - minimum 20 characters"
+    },
+    draft: {
+        message: "You have a saved draft. Continue writing?",
+        options:["Yes", "No"],
+        actions:["Yes", "No"]
+    },
+    editPost: {
+        message:"Quit editing post?",
+        options:["No", "Yes"],
+        actions:["No", "Yes"]
+    },
     Hide: {
       message: "Other users won't be able to see your post. Hide post?",
       options:["Cancel", "Hide"],
-      actions:["cancel", "hide-confirmed"]
+      actions:["Cancel", "Comfirm-Hide"]
     },
     Unhide: {
       message: "Post unhidden",
       options:["Cancel", "Unhide"],
-      actions:["Cancel", "Confirmed-Unhide"]
+      actions:["Cancel", "Confirm-Unhide"]
     },
     Delete:{
       message: "Are you sure you want to delete this post?",
-      options:["Cancel", "DELETE"],
+      options:["Cancel", "Delete"],
       actions:["Cancel", "Confirm-Delete"]
     },
   }
